0b387a3eb3d9208bba676dad955e25f60b90b856
Kirill Maslinsky <kirill@altlinux.org>
2021-04-21 09:54:10 +0300

    начальная версия наполнения сайта

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,6 +6,7 @@ SITENAME = 'DATAH. Data for Humanities'
 SITEURL = ''
 
 PATH = 'content'
+PAGE_PATHS = ['pages']
 
 TIMEZONE = 'Europe/Moscow'
 
@@ -29,6 +30,19 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
+DISPLAY_PAGES_ON_MENU = True
+
+MENUITEMS = [('Репозиторий', 'https://dataverse.datah.ru')]
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+THEME='nice-blog'
+#THEME='built-texts'
+LOAD_CONTENT_CACHE = False
+
+## nice-blog attributes
+LOGO = 'DATAH2.png'
+FAVICON = 'DATAH1.png'
+THEME_COLOR = 'red'
+SIDEBAR_DISPLAY = ['about', 'categories']
+SIDEBAR_ABOUT = 'DATAH — платформа для размещения открытых данных для исследований в области гуманитарных наук. Хостинг и техническое обслуживание платформы осуществляет Институт русской литературы (Пушкинский Дом) РАН. <a href="pages/about.html">Подробнее...</a>'
